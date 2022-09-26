02f5b5989dd9eabe8d79a23d974e65cdfe70504d
Gabriel Revillat <gabrielrevillat@yahoo.es>
2022-09-25 22:43:12 -0600

    F-4.4 Implemented view of password change screen.

---
+++ ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/ChangePasswordActivity.java
@@ -0,0 +1,14 @@
+package cr.ac.ucr.ecci.proyecto_arce_mall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
