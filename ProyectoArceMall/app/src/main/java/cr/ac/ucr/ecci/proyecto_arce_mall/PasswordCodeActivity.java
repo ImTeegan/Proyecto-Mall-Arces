670350e5098ffcf2ccca17a77ae1f1c40008241d
Gabriel Revillat <gabrielrevillat@yahoo.es>
2022-09-25 20:16:20 -0600

    F-4.4 Changed activity name. Implemented view of password code screen.

--- ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/PasswordCodeActivity.java
+++ ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/PasswordCodeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ChangePasswordActivity extends AppCompatActivity {
+public class PasswordCodeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_password);
+        setContentView(R.layout.activity_password_code);
     }
 }
