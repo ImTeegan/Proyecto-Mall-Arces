ec3bda23f46ea3d25c2ff0a5ef45cb9e91e89eac
Gabriel Revillat <gabrielrevillat@yahoo.es>
2022-09-28 17:16:16 -0600

    F-4.5 Cleaning code in ChangePasswordActivity

--- ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/ChangePasswordActivity.java
+++ ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/ChangePasswordActivity.java
@@ -14,7 +14,7 @@ import cr.ac.ucr.ecci.proyecto_arce_mall.data.model.User;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
-    private Button registrationButton;
+    private Button loginButton;
     private DbHelper dataBase;
     private TextInputLayout tilNewPassword;
     private TextInputLayout tilConfirmPassword;
@@ -24,12 +24,20 @@ public class ChangePasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
-        user = getIntent().getParcelableExtra("user");
+
+        this.instantiateComponents();
+    }
+
+    private void instantiateComponents() {
         this.dataBase = new DbHelper(this);
-        this.registrationButton = (Button) findViewById(R.id.login_button);
+        this.loginButton = (Button) findViewById(R.id.login_button);
         this.tilNewPassword = findViewById(R.id.til_new_password);
         this.tilConfirmPassword = findViewById(R.id.til_confirm_password);
-        registrationButton.setOnClickListener(new View.OnClickListener() {
+        this.user = getIntent().getParcelableExtra("user");
+    }
+
+    private void setComponentActions() {
+        this.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 validatePassword();
@@ -37,20 +45,26 @@ public class ChangePasswordActivity extends AppCompatActivity {
         });
     }
 
-    protected void validatePassword(){
+    protected void validatePassword() {
         String newPassword = tilNewPassword.getEditText().getText().toString();
         String confirmPassword = tilConfirmPassword.getEditText().getText().toString();
-        if(newPassword.equals(confirmPassword) && newPassword.length() > 8){
+
+        if (newPassword.equals(confirmPassword)
+            && newPassword.length() > 8) {
+            // Save new password.
             savePassword(user, newPassword);
-        }else{
-            this.tilConfirmPassword.setError("Las contraseñas deben ser igual y tener un minimo de 8 caracteres");
+        } else {
+            this.tilConfirmPassword.setError("Las contraseñas deben ser igual y" +
+                                             "tener un minimo de 8 caracteres");
         }
     }
 
-    protected void savePassword(User user, String newPassword){
+    protected void savePassword(User user, String newPassword) {
         user.setFirstTime(0);
         user.setPassword(newPassword);
-        dataBase.updateUser(user);
+
+        this.dataBase.updateUser(user);
+
         Intent intent = new Intent(this, StoreActivity.class);
         startActivity(intent);
         finish();
