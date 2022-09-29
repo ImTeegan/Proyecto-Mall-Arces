077ac4fafc1564b219b0c17fdb6a5dc477594202
Gabriel Revillat <gabrielrevillat@yahoo.es>
2022-09-28 19:41:27 -0600

    F-6.2 Trying to implement functionality of no internet connection.

--- ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/MainActivity.java
+++ ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/MainActivity.java
@@ -1,12 +1,20 @@
 package cr.ac.ucr.ecci.proyecto_arce_mall;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,40 +22,63 @@ import java.util.List;
 
 import cr.ac.ucr.ecci.proyecto_arce_mall.data.model.DbHelper;
 import cr.ac.ucr.ecci.proyecto_arce_mall.data.model.User;
+import cr.ac.ucr.ecci.proyecto_arce_mall.utility.Common;
 
 public class MainActivity extends AppCompatActivity {
+
+    private class NetworkChangeListener extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if(!Common.isConnectedToInternet(context)) {
+
+                this.goToMainScreen();
+
+                AlertDialog.Builder builder =  new AlertDialog.Builder(context);
+                View view = LayoutInflater.from(context)
+                                          .inflate(R.layout.no_internet_connection,null);
+                builder.setView(view);
+
+                AppCompatButton retryButton = view.findViewById(R.id.retry_button);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                dialog.setCancelable(false);
+                dialog.getWindow().setGravity(Gravity.CENTER);
+
+                retryButton.setOnClickListener(view1 -> {
+                    dialog.dismiss();
+                    onReceive(context,intent);
+                });
+            }
+        }
+
+        private void goToMainScreen() {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    NetworkChangeListener networkChangeListener = new NetworkChangeListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is a test
-        /*Button button = (Button) findViewById(R.id.loginButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AppCompatActivity Activity = MainActivity.this;
-                DbHelper Database = new DbHelper(Activity);
+    }
+    
+    @Override
+    protected void onStart() {
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(this.networkChangeListener,intentFilter);
+        super.onStart();
+    }
 
-               List<User> Users =  Database.getAllUser();
-
-               for(int i=0;i<Users.size();i++){
-                   User newUser = Users.get(i);
-                   Log.i("ID:" , newUser.getIdentification());
-                   Log.i("Name" , newUser.getName());
-                   Log.i("Email" , newUser.getEmail());
-                   Log.i("Province" , newUser.getProvince());
-                   Log.i("Birthday" , newUser.getBirthday());
-                   Log.i("Password",newUser.getPassword());
-               }
-
-               if(Database.checkUser("cesar@lopez.com","DEDOS")){
-                   Log.i("INGRESA CORRECTAMENTE","SI INGRESA");
-               };
-
-
-            }
-        });*/
+    @Override
+    protected void onStop() {
+        unregisterReceiver(this.networkChangeListener);
+        super.onStop();
     }
 
     public void showRegistrationScreen(View view) {
