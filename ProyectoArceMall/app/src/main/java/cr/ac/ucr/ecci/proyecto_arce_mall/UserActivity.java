8a916209076f5a8d44499faf8d3bb4377b5a0077
mj-chuz <medranom778@gmail.com>
2022-10-15 20:46:00 -0600

    F-2.4 implement the functionality to edit and save user data

--- ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/UserActivity.java
+++ ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/UserActivity.java
@@ -3,23 +3,26 @@ package cr.ac.ucr.ecci.proyecto_arce_mall;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.textfield.TextInputLayout;
 import com.reginald.editspinner.EditSpinner;
 
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 import cr.ac.ucr.ecci.proyecto_arce_mall.data.model.DbHelper;
 import cr.ac.ucr.ecci.proyecto_arce_mall.data.model.User;
@@ -34,6 +37,9 @@ public class UserActivity extends AppCompatActivity {
     private EditText userId;
     private EditText userEmail;
     private EditText userAge;
+    private Button updateUserButton;
+    private String newDate;
+    private Button changePasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +49,7 @@ public class UserActivity extends AppCompatActivity {
         this.instantiateComponents();
         this.setDropdownProvinces();
         this.setInformationFields();
+        this.setComponentActions();
 
         bottomNavigationView = findViewById(R.id.nav_view);
         bottomNavigationView.setSelectedItemId(R.id.navigation_user);
@@ -75,11 +82,40 @@ public class UserActivity extends AppCompatActivity {
     private void instantiateComponents() {
         this.dataBase = new DbHelper(this);
         this.activeUser = dataBase.getLoginUser();
-        this.provinceSpinner = (EditSpinner) findViewById(R.id.edit_spinner);
-        this.userId = (EditText) findViewById(R.id.user_id);
-        this.userName = (EditText) findViewById(R.id.user_name);
-        this.userEmail = (EditText) findViewById(R.id.email_user);
-        this.userAge = (EditText) findViewById(R.id.age_user);
+        this.provinceSpinner = findViewById(R.id.edit_spinner);
+        this.userId = findViewById(R.id.user_id);
+        this.userName = findViewById(R.id.user_name);
+        this.userEmail = findViewById(R.id.email_user);
+        this.userAge = findViewById(R.id.age_user);
+        this.updateUserButton = findViewById(R.id.update_button);
+        this.changePasswordButton = findViewById(R.id.change_password_button);
+    }
+
+    /**
+     * Set the actions for the buttons update data and change password
+     */
+    private void setComponentActions() {
+        this.updateUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    validateData();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        this.changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    showChangePasswordScreen();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     /**
@@ -100,18 +136,99 @@ public class UserActivity extends AppCompatActivity {
         userId.setText(this.activeUser.getIdentification());
         userName.setText(this.activeUser.getName());
         userEmail.setText(this.activeUser.getEmail());
-        userAge.setText(getAgeFromBirthdate() + " años");
+        userAge.setText(getAgeFromBirthdate(this.activeUser.getBirthday()) + " años");
+        this.userAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDatePickerDialog();
+            }
+        });
     }
 
     /**
      * Calculates the age based on the birth date
      * @return age The age of the user
      */
-    private String getAgeFromBirthdate(){
+    private String getAgeFromBirthdate(String date){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
-        LocalDate localDate = LocalDate.parse(this.activeUser.getBirthday(), formatter);
+        LocalDate localDate = LocalDate.parse(date, formatter);
         LocalDate curDate = LocalDate.now();
         String age = String.valueOf(Period.between(localDate, curDate).getYears());
         return age;
+    }
+
+    /**
+     * Validate the birthdate is a valid date before updating the user
+     * Updates the user with the new information
+     * @throws ParseException
+     */
+    private void validateData() throws ParseException {
+        String newBirthDate = this.newDate;
+        String newProvince = this.provinceSpinner.getText().toString();
+
+        if (validateBirthDate(newBirthDate)){
+            this.activeUser.setProvince(newProvince);
+            this.activeUser.setBirthday(newBirthDate);
+            this.dataBase.updateUser(this.activeUser);
+            Intent intent = new Intent(this, StoreActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+    /**
+     * Validate the birthdate date is a valid date
+     * @param birthDate the birthdate enter
+     * @return true if is a valid date and false if is not
+     * @throws ParseException
+     */
+    private boolean validateBirthDate(String birthDate) throws ParseException {
+        if (!birthDate.isEmpty()) {
+            LocalDate today = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+            LocalDate pastDate = LocalDate.parse(birthDate, formatter);
+            boolean isBefore = pastDate.isBefore(today);
+
+            if (!isBefore) {
+                this.userAge.setError("La fecha ingresada no es válida");
+            } else {
+                this.userAge.setError(null);
+            }
+            return isBefore;
+        }
+        this.userAge.setError("La fecha ingresada no es válida");
+        return false;
+    }
+
+    /**
+     * Opens the date picker dialog in order to pick a new birthdate
+     */
+    private void getDatePickerDialog() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                UserActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        newDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        userAge.setText(getAgeFromBirthdate(newDate) + " años");
+                    }
+                }, year, month, day);
+
+        datePickerDialog.show();
+    }
+
+    /**
+     * If the change password button is pressed, the screen to change it is shown
+     */
+    private void showChangePasswordScreen() {
+        Intent intent = new Intent(this, ChangePasswordActivity.class);
+        intent.putExtra("user", activeUser);
+        startActivity(intent);
     }
 }
