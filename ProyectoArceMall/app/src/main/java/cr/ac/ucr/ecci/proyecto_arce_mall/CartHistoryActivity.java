fbcf5b481523514b84ded2f9966c5fb338ca06b4
Gabriel Revillat <gabrielrevillat@yahoo.es>
2022-11-27 23:55:06 -0600

    F-6.1 Implemented view of cart history and purchase details.

---
+++ ProyectoArceMall/app/src/main/java/cr/ac/ucr/ecci/proyecto_arce_mall/CartHistoryActivity.java
@@ -0,0 +1,14 @@
+package cr.ac.ucr.ecci.proyecto_arce_mall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_history);
+    }
+}
